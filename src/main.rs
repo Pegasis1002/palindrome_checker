245237f25ac218aa9e2a83ea00b3ba5241e4e22b
Pegasis1002 <thewebserfer1234@gmail.com>
2024-04-30 17:49:35 +0530

    added some functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter a string: \n");
+    let mut in_string = String::new();
+    io::stdin()
+        .read_line(&mut in_string)
+        .expect("ERROR: input NOT a string.");
+
+    no_of_char(in_string);
+}
+
+fn no_of_char(in_string: String) {
+    let x = in_string.len();
+    if (x % 2) > 0 {
+        println!("'{}' has even number of chars", in_string);
+    } else if (x % 2) == 0 {
+        println!("'{}' has odd number of chars", in_string);
+    }
 }
